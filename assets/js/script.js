99899e060796236f8d604575f799dc8a6fbc9fbf
Ellen Lo <lowing@bu.edu>
2019-01-22 07:31:59 -0500

    mobile ver + poster position fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,17 @@ var prevMouseX = -1;
 var prevMouseY = -1;
 var path;
 
+function isMobile() {
+  var md = new MobileDetect(window.navigator.userAgent);
+  if(md.mobile()){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var mobileMode = isMobile();
+
 paper.install(window);
 window.onload = function() {
 
@@ -15,6 +26,7 @@ window.onload = function() {
   });
 
   // Set up event listeners for pseudo-img class elements
+  if(!mobileMode) {
   var pseudoimg = $('.pseudo-img');
   pseudoimg.each(function(index) {
     $(this).bind( {
@@ -57,7 +69,7 @@ window.onload = function() {
           $('#img-' + (imgSelectId+1).toString()).css({
             'left': transformX,
             'top' : transformY
-          })
+          });
         }
       },
       mouseup: function(e) {
@@ -70,6 +82,68 @@ window.onload = function() {
       }
     });
   });
+  } else {
+    var pseudoimg = document.getElementsByClassName('pseudo-img');
+    for(var i = 0; i < pseudoimg.length; i++) {
+      var pseudo = pseudoimg[i];
+
+      // touchstart
+      pseudo.addEventListener('touchstart', (event) => {
+        var target = event.target;
+        imgSelected = true;
+        imgSelectId = target.id.substring(target.id.length-1, target.id.length)-1;
+        var touch = event.targetTouches[0];
+        prevMouseX = touch.clientX - target.offsetLeft;
+        prevMouseY = touch.clientY - target.offsetTop;
+
+        // Sort depth of images
+        var pseudoSelectZ = target.style.zIndex;
+        var selectZ = $('#img-' + (imgSelectId+1).toString()).css('z-index');
+        $('.pseudo-img').each( function(index) {
+          if(index === imgSelectId) {
+            $(this).css('z-index', '98');
+          } else if ( $(this).css('z-index') > pseudoSelectZ ) {
+            var z = $(this).css('z-index')-1;
+            $(this).css('z-index', z.toString());
+          }
+        });
+        $('.img').each( function(index) {
+          if(index === imgSelectId) {
+            $(this).css('z-index', '8');
+          } else if ( $(this).css('z-index') > selectZ ) {
+            var z = $(this).css('z-index')-1;
+            $(this).css('z-index', z.toString());
+          }
+        });
+      }, false);
+
+      // touchmove
+      pseudo.addEventListener('touchmove', (event) => {
+        if(imgSelected && imgSelectId === event.target.id.substring(event.target.id.length-1, event.target.id.length)-1) {
+          var touch = event.targetTouches[0];
+          var currMouseX = touch.clientX;
+          var currMouseY = touch.clientY;
+          var transformX = currMouseX - prevMouseX;
+          var transformY = currMouseY - prevMouseY;
+          $('#pseudo-img-' + (imgSelectId+1).toString()).css({
+            'left': transformX,
+            'top' : transformY
+          });
+          $('#img-' + (imgSelectId+1).toString()).css({
+            'left': transformX,
+            'top' : transformY
+          });
+        }
+        console.log('touch move');
+      }, false);
+
+      // touchend
+      pseudo.addEventListener('touchend', (event) => {
+        imgSelected = false;
+        imgSelectId = -1;
+      }, false);
+    }
+  }
 
   // Initialize slidebars
   ( function ( $ ) {
@@ -99,14 +173,6 @@ window.onload = function() {
       }
     });
 
-    $('.info-bar').on('scroll', (e) => {
-      e.stopPropagation();
-      e.preventDefault();
-      if(infobarOpened) {
-        $('.info-bar').scrollTop(200);
-        console.log('scroll');
-      }
-    });
   } ) ( jQuery );
 
   // Set up drawing canvas
